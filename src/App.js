56ecbb8aa3583d3dc7dc2e773b06f70a2c7a2655
unknown <jburto94@yahoo.com>
2022-09-14 21:09:20 -0400

    added basic drag and drop list and functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as MovementIcon } from './components/misc/movement.svg';
+import { ReactComponent as Trash } from './components/misc/trash.svg';
+import { useState, useRef } from 'react';
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.scss';
+
+const defaultList = ['Jake', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott', 'Maya', 'Julie', 'Scott'];
+
+const App = () => {
+  const [list, setList] = useState(defaultList);
+  let dragItem = useRef();
+  let dragOverItem = useRef();
+
+  const dragStart = (e, rank) => {
+    dragItem.current = rank;
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+  }
+
+  const dragOver = (e, rank) => {
+    dragOverItem.current = rank;
+
+    if (dragItem.current === dragOverItem.current) {
+      return;
+    }
+
+    const arrangedList = [...list];
+    const draggedItem = arrangedList[dragItem.current];
+    arrangedList.splice(dragItem.current, 1)
+    arrangedList.splice(dragOverItem.current, 0, draggedItem);
+    setList(arrangedList);
+  }
+
+  const dropItem = e => {
+    dragItem.current = null;
+    dragOverItem.current = null;
+  }
+
+  const removeItem = (rank) => {
+    const updatedList = list.filter((item, idx) => idx !== rank)
+    setList(updatedList)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>List</h2>
+      <ul className='bg-light py-5'>
+        {list.map((item, idx)=> (
+          <li key={idx}>
+            <div>
+              <span className='rank'>{idx + 1}</span>
+              <div 
+                className='drag' 
+                draggable 
+                onDragStart={e => dragStart(e, idx) }
+                onDragEnter={e => dragOver(e, idx)}
+                onDragEnd={e => dropItem(e)}
+              >
+                <MovementIcon />
+              </div>
+              <span>{item}</span>
+              </div>
+            <div 
+              className='trash'
+              onClick={() => removeItem(idx)}
+            >
+              <Trash />
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
