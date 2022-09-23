c51cb81e9552688b1e815d36a8d4e57c3f6f4ade
Jake Burton <jburto94@yahoo.com>
2022-09-23 14:28:11 -0400

    Cleaned up a lot of the styles

--- src/components/partials/Footer/Footer.js
+++ src/components/partials/Footer/Footer.js
@@ -2,8 +2,9 @@ import './Footer.scss';
 
 const Footer = () =>  (
   <footer className="py-5 bg-dark Footer">
-    <div className="container">
-      <span className="text-white">Github</span>
+    <div className="container d-flex flex-wrap justify-content-between">
+      <a href='https://github.com/jburto94/drag-and-drop-ranker-client' className="text-white">See it on Github</a>
+      <span className="text-white">Created by <a href='//jakeburton.dev'>Jake Burton</a></span>
     </div>
   </footer>
 );
